9e814498c5fb7804a73bf64bdedc731d279908cf
Marcos <marcospaulobitt@gmail.com>
2026-05-27 18:16:35 -0300

    feat : visual dos feeds

--- ingressae/src/app/app.routes.ts
+++ ingressae/src/app/app.routes.ts
@@ -5,43 +5,47 @@ import { Perfil } from './pages/perfil/perfil';
 import { Lista } from './pages/fas-clube/lista/lista';
 import { Detalhe } from './pages/fas-clube/detalhe/detalhe';
 import { Fila } from './pages/fila/fila';
-
+import { Feed } from './pages/fas-clube/feed/feed';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'inicio',
-        pathMatch: 'full'
-    },
-    {
-        path: 'inicio',
-        component: Inicio
-    },
-    {
-        path: 'fila/:showId/preferencial',
-        component: Fila,
-        // canActivate: [authGuard]
-    },
-    {
-        path: 'fila/:showId/normal',
-        component: Fila,
-        // canActivate: [authGuard]
-    },
-    {
-        path: 'perfil',
-        component: Perfil,
-        // canActivate: [authGuard]
-    },
-    {
-        path: 'fas-clubes',
-        component: Lista
-    },
-    {
-        path: 'fas-clubes/:id',
-        component: Detalhe
-    },
-    {
-        path: '**',
-        redirectTo: 'inicio'
-    }
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
+  },
+  {
+    path: 'inicio',
+    component: Inicio,
+  },
+  {
+    path: 'fila/:showId/preferencial',
+    component: Fila,
+    // canActivate: [authGuard]
+  },
+  {
+    path: 'fila/:showId/normal',
+    component: Fila,
+    // canActivate: [authGuard]
+  },
+  {
+    path: 'perfil',
+    component: Perfil,
+    // canActivate: [authGuard]
+  },
+  {
+    path: 'fas-clubes',
+    component: Lista,
+  },
+  {
+    path: 'fas-clubes/:id',
+    component: Detalhe,
+  },
+  {
+    path: 'feed',
+    component: Feed,
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio',
+  },
 ];
--- ingressae/src/app/pages/fas-clube/feed/feed.ts
+++ ingressae/src/app/pages/fas-clube/feed/feed.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Banner } from './banner/banner';
+import { Comentarios } from '../comentarios/comentarios';
 
 @Component({
   selector: 'app-feed',
-  imports: [],
+  imports: [Banner, Comentarios],
   templateUrl: './feed.html',
   styleUrl: './feed.scss',
 })
-export class Feed {
-
-}
+export class Feed {}
