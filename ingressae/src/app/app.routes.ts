a79fb35d4513dcf074f9fe98ca5a0e2c9c30075f
marcos_p_medeiros <marcospaulobitt@gmail.com>
2026-06-10 20:06:44 -0300

    fix: alguem apagou as rotas

--- ingressae/src/app/app.routes.ts
+++ ingressae/src/app/app.routes.ts
@@ -5,39 +5,44 @@ import { Perfil } from './pages/perfil/perfil';
 import { Lista } from './pages/fas-clube/lista/lista';
 import { Detalhe } from './pages/fas-clube/detalhe/detalhe';
 import { Compra } from './pages/compra/compra';
+import { Feed } from './pages/fas-clube/feed/feed';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'inicio',
-        pathMatch: 'full'
-    },
-    {
-        path: 'inicio',
-        component: Inicio
-    },
-    {
-        path: 'fila/:showId/:tipo',
-        component: Fila
-    },
-    {
-        path: 'perfil',
-        component: Perfil
-    },
-    {
-        path: 'fas-clubes',
-        component: Lista
-    },
-    {
-        path: 'fas-clubes/:id',
-        component: Detalhe
-    },
-    {
-        path: 'compra/:showId/:tipo',
-        component: Compra
-    },
-    {
-        path: '**',
-        redirectTo: 'inicio'
-    }
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
+  },
+  {
+    path: 'inicio',
+    component: Inicio,
+  },
+  {
+    path: 'feed',
+    component: Feed,
+  },
+  {
+    path: 'fila/:showId/:tipo',
+    component: Fila,
+  },
+  {
+    path: 'perfil',
+    component: Perfil,
+  },
+  {
+    path: 'fas-clubes',
+    component: Lista,
+  },
+  {
+    path: 'fas-clubes/:id',
+    component: Detalhe,
+  },
+  {
+    path: 'compra/:showId/:tipo',
+    component: Compra,
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio',
+  },
 ];
