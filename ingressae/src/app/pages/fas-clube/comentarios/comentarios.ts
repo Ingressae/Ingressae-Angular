e6c283bbe3f6375f2267d4f570a421bbe2bc1874
marcos_p_medeiros <marcospaulobitt@gmail.com>
2026-06-10 19:51:53 -0300

    fix : like melhorado

--- ingressae/src/app/pages/fas-clube/comentarios/comentarios.ts
+++ ingressae/src/app/pages/fas-clube/comentarios/comentarios.ts
@@ -1,14 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { Comentario } from '../../../models/comentario';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 import { MarkdownPipe } from '../../../shared/pipes/markdown-pipe';
 
 @Component({
   selector: 'app-comentarios',
-  imports: [DatePipe, MarkdownPipe],
+  imports: [DatePipe, MarkdownPipe, NgClass],
   templateUrl: './comentarios.html',
   styleUrl: './comentarios.scss',
 })
 export class Comentarios {
   @Input() comentario!: Comentario;
+  curtido: boolean = true;
+
+  curtir() {
+    this.curtido = !this.curtido;
+    this.comentario.qtdlike++;
+  }
+
+  descurtir() {
+    this.curtido = !this.curtido;
+    this.comentario.qtdlike--;
+  }
 }
