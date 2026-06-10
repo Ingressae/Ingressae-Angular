8e23be338c8ba62ae1f3557491a5ed105a16e02b
marcos_p_medeiros <marcospaulobitt@gmail.com>
2026-06-09 21:18:17 -0300

    fix : arrumada a formatação para pegar do começo se precisar

--- ingressae/src/app/shared/pipes/markdown-pipe.ts
+++ ingressae/src/app/shared/pipes/markdown-pipe.ts
@@ -9,11 +9,38 @@ export class MarkdownPipe implements PipeTransform {
       return '';
     }
 
-    return texto
-      .replace(/\*(.*?)\*/g, '<strong>$1</strong>')
-      .replace(/_(.*?)_/g, '<em>$1</em>')
-      .replace(/~~(.*?)~~/g, '<del>$1</del>')
-      .replace(/`(.*?)`/g, '<code>$1</code>')
-      .replace(/\n/g, '<br>');
+    texto = this.processar(texto, '*', 'strong');
+    texto = this.processar(texto, '_', 'em');
+    texto = this.processar(texto, '~~', 'del');
+    texto = this.processar(texto, '`', 'code');
+
+    return texto.replace(/\n/g, '<br>');
+  }
+
+  private processar(texto: string, marcador: string, tag: string): string {
+    const partes = texto.split(marcador);
+
+    if (partes.length <= 1) {
+      return texto;
+    }
+
+    let resultado = partes[0];
+
+    for (let i = 1; i < partes.length; i++) {
+      const ultimo = i === partes.length - 1;
+      const aberto = i % 2 !== 0;
+
+      if (aberto) {
+        if (ultimo) {
+          resultado += `<${tag}>${partes[i]}</${tag}>`;
+        } else {
+          resultado += `<${tag}>${partes[i]}</${tag}>`;
+        }
+      } else {
+        resultado += partes[i];
+      }
+    }
+
+    return resultado;
   }
 }
